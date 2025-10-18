d94ea429a0313549330ef9ffb221dd52604ab246
Alfonso <espinozalfonsoch@gmail.com>
2025-10-17 20:21:37 -0500

    Hola.

---
+++ src/main/java/com/example/encriptacion_aes/EncriptacionAesApplication.java
@@ -0,0 +1,13 @@
+package com.example.encriptacion_aes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncriptacionAesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncriptacionAesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/encriptacion_aes/EncriptacionAesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encriptacion_aes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncriptacionAesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
